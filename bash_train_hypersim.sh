042ebf24f91ecfccc0a26d5e5ba07329b0692c44
houchenfeng <1208262656@qq.com>
2025-07-02 06:56:45 +0000

    * use diff-gaussian-rasterization-softmax

--- bash_train_hypersim.sh
+++ bash_train_hypersim.sh
@@ -1,18 +1,27 @@
 # pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
 
 # List of scenes
-spatialgen_scenes=(
-    'ai_047_006'
+hypersim_scenes=(
+    'ai_003_006'
+    'ai_014_010'
+    'ai_016_007'
+    'ai_017_005'
+    # 'ai_017_006'
+    # 'ai_018_003'
+    # 'ai_018_008'
+    # 'ai_037_008'
+    # 'ai_039_006'
+    # 'ai_043_005'
 )
 # Loop through each scene 
-for i in "${!spatialgen_scenes[@]}"; do
-    scene="${spatialgen_scenes[$i]}"
+for i in "${!hypersim_scenes[@]}"; do
+    scene="${hypersim_scenes[$i]}"
 
     echo "Reconstructing scene: $scene"
 
     python3 train.py \
-        --source_path data/spatialgen-hypersim-eval/val/$scene \
-        --model_path output/spatialgen-hypersim/${scene}_warp20_fixed \
+        --source_path data/spatialgen-hypersim-eval-32view/val/$scene \
+        --model_path output/spatialgen-hypersim-32view/${scene}_warp20_sparsegs \
         --beta 5.0 \
         --lambda_pearson 0.05 \
         --lambda_local_pearson 0.15 \
@@ -30,8 +39,8 @@ for i in "${!spatialgen_scenes[@]}"; do
         --warp_reg_start_itr 100
 
     python3 render.py \
-        --source_path data/spatialgen-hypersim-eval/val/$scene \
-        --model_path output/spatialgen-hypersim/${scene}_warp20_fixed \
+        --source_path data/spatialgen-hypersim-eval-32view/val/$scene \
+        --model_path output/spatialgen-hypersim-32view/${scene}_warp20_sparsegs \
         --no_load_depth \
         --iteration 30000
 
--- bash_train_spatialgen.sh
+++ bash_train_spatialgen.sh
@@ -3,15 +3,15 @@
 # List of scenes
 spatialgen_scenes=(
     '20241106_3FO4K5HXIXL1_perspective_room_279'
-    '20241106_3FO4K5HXIXL1_perspective_room_281'
-    '20241106_3FO4K5HXJBY5_perspective_room_240'
+    # '20241106_3FO4K5HXIXL1_perspective_room_281'
+    # '20241106_3FO4K5HXJBY5_perspective_room_240'
     '20241118_3FO4K5G1C7DO_perspective_room_507'
     '20241118_3FO4K5FXC4C9_perspective_room_821'
-    '20241118_3FO4K5FXFJPA_perspective_room_812'
-    '20241118_3FO4K5FXGOTM_perspective_room_1116'
-    '20241118_3FO4K5G1CO3T_perspective_room_628'
-    '20241118_3FO4K5G1EOBF_perspective_room_585'
-    '20241118_3FO4K5G1X4MX_perspective_room_1112'
+    # '20241118_3FO4K5FXFJPA_perspective_room_812'
+    # '20241118_3FO4K5FXGOTM_perspective_room_1116'
+    # '20241118_3FO4K5G1CO3T_perspective_room_628'
+    # '20241118_3FO4K5G1EOBF_perspective_room_585'
+    # '20241118_3FO4K5G1X4MX_perspective_room_1112'
 )
 # Loop through each scene 
 for i in "${!spatialgen_scenes[@]}"; do
@@ -20,8 +20,8 @@ for i in "${!spatialgen_scenes[@]}"; do
     echo "Reconstructing scene: $scene"
 
     python3 train.py \
-        --source_path data/spatialgen-spatialgen-eval/val/$scene \
-        --model_path output/spatialgen-spatialgen/${scene}_warp20_fixed \
+        --source_path data/spatialgen-spatialgen-eval-16view/val/$scene \
+        --model_path output/spatialgen-spatialgen-16view/${scene}_warp20_sparsegs \
         --beta 5.0 \
         --lambda_pearson 0.05 \
         --lambda_local_pearson 0.15 \
@@ -39,8 +39,8 @@ for i in "${!spatialgen_scenes[@]}"; do
         --warp_reg_start_itr 100
 
     python3 render.py \
-        --source_path data/spatialgen-spatialgen-eval/val/$scene \
-        --model_path output/spatialgen-spatialgen/${scene}_warp20_fixed \
+        --source_path data/spatialgen-spatialgen-eval-16view/val/$scene \
+        --model_path output/spatialgen-spatialgen-16view/${scene}_warp20_sparsegs \
         --no_load_depth \
         --iteration 30000
 
